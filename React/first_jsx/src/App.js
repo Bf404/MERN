641ab55e78a01be6ea19458a57f4073ad155d470
Waleed Almotairi <AlmotairiWal@gmail.com>
2021-05-26 18:23:11 +0300

    First React

---
+++ React/first_jsx/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/first_jsx/src/components/Hello.jsx
@@ -0,0 +1,21 @@
+import React, {Component } from 'react'
+
+class Hello extends Component {
+    render() {
+        return ( <>
+            <h1> Hello Dojo</h1>
+            <h2>Things I need to do:</h2>
+            <u>
+                <li>Learn React.</li>
+                <li>Get a job.</li>
+                <li>Find nearby Gym.</li>
+                <li>Learn C#.</li>
+
+            </u>
+        </>
+
+        )
+    }
+}
+
+export default Hello;
